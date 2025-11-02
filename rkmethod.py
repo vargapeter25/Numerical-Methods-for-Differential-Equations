468c8b8210b3eab7d49d1d71e35bb4da3766e388
Varga Peter <varga.peter2500@gmail.com>
2025-11-02 13:01:17 +0100

    Added order approximation

--- rkmethod.py
+++ rkmethod.py
@@ -13,9 +13,13 @@ class Result:
     grid: np.ndarray
     y: np.ndarray
 
+    @property
+    def size(self) -> int:
+        return len(self.grid)
+
     def get_sample(self, sample_size: int) -> Any:
         assert (len(self.grid) - 1) % (sample_size - 1) == 0
-        step = (len(self.grid) - 1) / (sample_size - 1)
+        step = (len(self.grid) - 1) // (sample_size - 1)
         return Result(self.step_size * step,
                           self.grid[0:len(self.grid):step],
                           self.y[0:len(self.y):step])
@@ -187,6 +191,7 @@ class RKMethod:
         print(output)
         self.print_stability_function()
         self.plot_stability_region()
+        # If used outside of jupyter: plt.show()
 
     def get_nodepy_rkm(self) -> Any:
         return nrk.ExplicitRungeKuttaMethod(self.A, self.b, name = self.name) if self.is_explicit() else nrk.RungeKuttaMethod(self.A, self.b, name = self.name)
@@ -229,21 +234,184 @@ class RKMethod:
 
         :return: The maximums order.
         """
+
         assert self.stages >= 1
 
         if self.is_explicit():
             min_num_of_stages = np.array([1, 2, 3, 4, 6, 7, 9, 11])
             if self.stages <= 11:
                 return np.searchsorted(min_num_of_stages, self.stages, side='right')
+            
+            print('Warning: Stage number is too high for exact result.')
             return self.stages - 2
         
         return self.stages * 2
+    
+
+    def order_from_exact_solution(self, f: Any, y_0: np.ndarray, t_0: float, t_f: float, steps: list[int], f_sol: Any, dim_sol: int, dims: np.ndarray | None = None, norm_ord : Any = np.inf) -> np.ndarray:
+        """ Estimates the order of consistency from the exact solution.
+
+        Calculates the `E(h_i)` from based on `steps` and the exact solution, and calculates the estimate based on adjacent values.
         
-    # TODO: A-stability region + function (+ is A-stable), possible maximal order, order, order guessing form test
+        :param f: The function defining the ODE. f must have args in `(t, u)` order.
+        :param y_0: The initial value at `t_0`. Must be an `np.array`.
+        :param t_0: The beginning of the time intervall.
+        :param t_f: The end of the time intervall.
+        :param steps: The number of steps per calculations used in the estimates.
+        :param f_sol: The exact solution. f must have an argument `t`.
+        :param dim_sol: The dimension of the solution vector.
+        :param dims: The dimension used for estimate calcualtion. It must have size `dim_sol`.
+        :param norm_ord: The norm used for the approximation. It uses `numpy.norm`.
+        :return: An array of the estimates where each column corresponds to one of the coordinates.
+        """
+        if dims is None:
+            dims = np.arange(dim_sol)
+        else:
+            dims = np.array(dims)
+
+        assert len(np.shape(dims)) == 1
+        assert dim_sol == np.size(dims)
+
+        errors = []
+        hs = []
+        for N in steps:
+            exact = get_exact_result(f_sol, dim_sol, t_0, t_f, N)
+            rk_sol = self.solver(f, y_0, t_0, t_f, N)
+
+            assert np.shape(rk_sol.y[:,dims]) == np.shape(exact.y)
+
+            hs.append(rk_sol.step_size)
+            error = np.linalg.norm(rk_sol.y[:,dims] - exact.y, axis=0, ord=norm_ord)
+            # Discrete p-norm | if p is not inf, than
+            if isinstance(norm_ord, int):
+                error *= rk_sol.step_size**(1./norm_ord)
+            errors.append(error)
+
+        result = []
+        for i in range(len(errors) - 1):
+            result.append(np.log(errors[i] / errors[i+1]) / np.log(hs[i] / hs[i+1]))
+
+        return np.array(result)
+
+
+    def order_from_fine_grid(self, f: Any, y_0: np.ndarray, t_0: float, t_f: float, steps: list[int], fine_step: int, dims: np.ndarray | None = None, norm_ord : Any = np.inf) -> np.ndarray:
+        """ Estimates the order of consistency from a fine grid.
+
+        Calculates the `E(h_i)` from based on `steps` and a fine grid defined by `fine_step`, and calculates the estimate based on adjacent values.
+        
+        :param f: The function defining the ODE. f must have args in `(t, u)` order.
+        :param y_0: The initial value at `t_0`. Must be an `np.array`.
+        :param t_0: The beginning of the time intervall.
+        :param t_f: The end of the time intervall.
+        :param steps: The number of steps per calculations used in the estimates.
+        :param fine_step: The number of steps in the fine grid. 
+        :param dims: The dimension used for estimate calcualtion. It must have size `dim_sol`.
+        :param norm_ord: The norm used for the approximation. It uses `numpy.norm`.
+        :return: An array of the estimates where each column corresponds to one of the coordinates.
+        """
+        if dims is None:
+            dims = np.arange(len(y_0))
+        else:
+            dims = np.array(dims)
+
+        fine_grid_sol = self.solver(f, y_0, t_0, t_f, fine_step)
+
+        errors = []
+        hs = []
+        for N in steps:
+            rk_sol = self.solver(f, y_0, t_0, t_f, N)
+            exact = fine_grid_sol.get_sample(len(rk_sol.grid))
+            error = np.linalg.norm(rk_sol.y[:, dims] - exact.y[:, dims], axis=0, ord=norm_ord)
+            hs.append(rk_sol.step_size)
+            # Discrete p-norm | if p is not inf, than
+            if isinstance(norm_ord, int):
+                error *= rk_sol.step_size**(1./norm_ord)
+            errors.append(error)
+
+        result = []
+        for i in range(len(errors) - 1):
+            result.append(np.log(errors[i] / errors[i+1]) / np.log(hs[i] / hs[i+1]))
+
+        return np.array(result)
+
+
+    def order_from_coarse_grid_1(self, f: Any, y_0: np.ndarray, t_0: float, t_f: float, N: int, dims: np.ndarray | None = None, norm_ord : Any = np.inf) -> np.ndarray:
+        """ Estimates the order of consistency from a coarse grid.
+
+        Calculates the `E(h_i)` for `N`, `2N` and `4N`, approximates with the adjacent values.
+        
+        :param f: The function defining the ODE. f must have args in `(t, u)` order.
+        :param y_0: The initial value at `t_0`. Must be an `np.array`.
+        :param t_0: The beginning of the time intervall.
+        :param t_f: The end of the time intervall.
+        :param N: The number of steps in the original method.
+        :param dims: The dimension used for estimate calcualtion. It must have size `dim_sol`.
+        :param norm_ord: The norm used for the approximation. It uses `numpy.norm`.
+        :return: An array of the estimates where each column corresponds to one of the coordinates.
+        """
+        if dims is None:
+            dims = np.arange(len(y_0))
+        else:
+            dims = np.array(dims)
+
+        rk_sols = []
+        for i in range(3):
+            rk_sols.append(self.solver(f, y_0, t_0, t_f, N * 2**i))
+
+        errors = []
+        for i in range(2):
+            exact = rk_sols[i + 1].get_sample(rk_sols[i].size).y[:,dims]
+            error = np.linalg.norm(rk_sols[i].y[:,dims] - exact, axis=0, ord=norm_ord)
+            # Discrete p-norm | if p is not inf, than
+            if isinstance(norm_ord, int):
+                error *= rk_sols[i].step_size**(1./norm_ord)
+            errors.append(error)
+
+        order = np.log2(errors[0] / errors[1])
+        return order
+
+
+    def order_from_coarse_grid_2(self, f: Any, y_0: np.ndarray, t_0: float, t_f: float, N: int, dims: np.ndarray | None = None, norm_ord : Any = np.inf) -> np.ndarray:
+        """ Estimates the order of consistency from a coarse grid.
+
+        Calculates the `E(h_i)` for `N`, `2N` and `4N`, approximates with the finest grid.
+        
+        :param f: The function defining the ODE. f must have args in `(t, u)` order.
+        :param y_0: The initial value at `t_0`. Must be an `np.array`.
+        :param t_0: The beginning of the time intervall.
+        :param t_f: The end of the time intervall.
+        :param N: The number of steps in the original method.
+        :param dims: The dimension used for estimate calcualtion. It must have size `dim_sol`.
+        :param norm_ord: The norm used for the approximation. It uses `numpy.norm`.
+        :return: An array of the estimates where each column corresponds to one of the coordinates.
+        """
+        if dims is None:
+            dims = np.arange(len(y_0))
+        else:
+            dims = np.array(dims)
+
+        rk_sols = []
+        for i in range(3):
+            rk_sols.append(self.solver(f, y_0, t_0, t_f, N * 2**i))
+
+        errors = []
+        for i in range(2):
+            exact = rk_sols[2].get_sample(rk_sols[i].size).y[:,dims]
+            error = np.linalg.norm(rk_sols[i].y[:,dims] - exact, axis=0, ord=norm_ord)
+            # Discrete p-norm | if p is not inf, than
+            if isinstance(norm_ord, int):
+                error *= rk_sols[i].step_size**(1./norm_ord)
+            errors.append(error)
+
+        order = np.log2(errors[0] / errors[1] - 1)
+        return order
+
 
 def get_exact_result(f: Any, dim: int, t_0: float, t_f: float, N: int) -> Result:
     """ Evaluates the `f` function in the grid points.
     
+    f must return an np.ndarray even if the problem is 1 dimensional.
+
     :param f: The function.
     :param dim: The dimension of the function param.
     :param t_0: The beginning of the time intervall.
@@ -270,3 +438,4 @@ class RKMethods():
     IE          = RKMethod(np.array([[1]]), np.array([1]), np.array([1]), name='Implicit Euler')
     TRAPEZOIDAL = RKMethod(np.array([[0, 0], [1/2, 1/2]]), np.array([1/2, 1/2]), np.array([0, 1]), name='Trapezoidal')
     RK2         = RKMethod(np.array([[0, 0], [0.5, 0]]), np.array([0, 1]), np.array([0, 0.5]), name='RK2')
+    RK4         = RKMethod(np.array([[0, 0, 0, 0], [0.5, 0, 0, 0], [0, 0.5, 0, 0], [0, 0, 1, 0]]), np.array([1/6, 1/3, 1/3, 1/6]), np.array([0, 0.5, 0.5, 1]), name='RK4')
